abaef4c49af7caaa29c1346421d1674a60cf462c
Oladayo Osikoya <alwaysdeone@gmail.com>
2017-03-20 21:09:55 +0000

    Create Account model.

--- accounts/models.py
+++ accounts/models.py
@@ -1,5 +1,8 @@
 from __future__ import unicode_literals
 
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Account(models.Model):
+    user = models.OneToOneField(User)
+    wallet_address = models.CharField(max_length=35)
