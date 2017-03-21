7ca1e68cecb2bdea03eecc60c125b1251cdf01bd
Oladayo Osikoya <alwaysdeone@gmail.com>
2017-03-21 11:13:26 +0000

    Save user and redirect to home page.

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,8 +1,13 @@
 from django import forms
 from django.conf import settings
+from django.contrib.auth.models import User
 from django.utils.translation import ugettext_lazy as _
 
+from .models import Account
+
 from nocaptcha_recaptcha.fields import NoReCaptchaField
+
+import requests
 
 class CreateAccountForm(forms.Form):
     full_name = forms.CharField(label=_('Full Name'), widget=forms.TextInput(attrs={
@@ -28,14 +33,39 @@ class CreateAccountForm(forms.Form):
     captcha = NoReCaptchaField()
     agree_to_terms = forms.BooleanField()
 
-    """ def clean_name(self):
+    def clean_full_name(self):
         cleaned_data = super(CreateAccountForm, self).clean()
-        name = cleaned_data.get('name')
+        full_name = cleaned_data.get('full_name')
 
-        if len(name.split(' ')) == 1:
+        if len(full_name.split(' ')) == 1:
             raise forms.ValidationError(_('Enter first and last names'), code='incomplete_name')
 
-        return name
+        return full_name
+
+    """ def clean_captcha(self):
+        cleaned_data = super(CreateAccountForm, self).clean()
+        captcha = cleaned_data.get('captcha')
+        print captcha
+
+        response = requests.post(settings.NORECAPTCHA_VERIFY_URL, data={
+            'secret': settings.NORECAPTCHA_SECRET_KEY,
+            'response': captcha
+        })
+
+        print response.status_code, response.json()
+        # 200 {u'hostname': u'btc-deone.c9users.io', u'challenge_ts': u'2017-03-20T17:06:10Z', u'success': False}
+
+        return captcha """
+
+    def clean_wallet_address(self):
+        cleaned_data = super(CreateAccountForm, self).clean()
+        wallet_address = cleaned_data.get('wallet_address')
+        address_length = len(wallet_address)
+
+        if address_length < 26 or address_length > 35 or wallet_address[0] not in ['1', '3']:
+            raise forms.ValidationError(_('Invalid wallet address'), code='invalid_address')
+
+        return wallet_address
 
     def clean(self):
         cleaned_data = super(CreateAccountForm, self).clean()
@@ -44,7 +74,18 @@ class CreateAccountForm(forms.Form):
 
         if password and confirm_password:
             if password != confirm_password:
-                raise forms.ValidationError(_('Passwords do not match'), code='password_mismatch') """
+                raise forms.ValidationError(_('Passwords do not match'), code='password_mismatch')
 
     def save(self):
-        print self.cleaned_data
+        first_name, last_name = self.cleaned_data['full_name'].split(' ')
+        email = self.cleaned_data['email']
+        password = self.cleaned_data['password']
+
+        user = User.objects.create_user(email, email, password)
+        user.first_name = first_name
+        user.last_name = last_name
+        user.save()
+
+        account = Account.objects.create(user=user, wallet_address=self.cleaned_data['wallet_address'])
+
+        return user
--- accounts/views.py
+++ accounts/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views.generic.base import TemplateView
 
 from .forms import CreateAccountForm
@@ -11,10 +11,11 @@ def create(request):
     if request.method == 'POST':
         form = CreateAccountForm(request.POST, label_suffix='')
         if form.is_valid():
-            form.save()
+            user = form.save()
             # send mail
             # log user in
             # redirect
+            return redirect('home')
     else:
         form = CreateAccountForm(label_suffix='')
 
