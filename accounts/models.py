c55fc55fc2fd031a5502259d607207a8f3ae7ddf
Oladayo Osikoya <alwaysdeone@gmail.com>
2017-03-22 14:08:58 +0000

    Send email after saving user. Use index.html for homepage and accounts/index.html from user home.

--- accounts/models.py
+++ accounts/models.py
@@ -1,6 +1,7 @@
 from __future__ import unicode_literals
 
 from django.db import models
+from django.conf import settings
 from django.dispatch import receiver
 from django.core.mail import send_mail
 from django.contrib.auth.models import User
@@ -13,11 +14,14 @@ class Account(models.Model):
 @receiver(post_save, sender=User)
 def send_welcome_email(sender, **kwargs):
     user = kwargs['instance']
-    if user.first_name:
+    update_fields = kwargs['update_fields']
+
+    # Send email after user logs in and last_login attribute is updated.
+    if update_fields is not None:
         send_mail(
             'Subject here',
             'Here is the message.',
-            'from@example.com',
-            ['alwaysdeone@gmail.com'],
+            settings.DEFAULT_FROM_EMAIL,
+            [user.email],
             fail_silently=False,
         )
--- accounts/views.py
+++ accounts/views.py
@@ -1,21 +1,30 @@
 from django.shortcuts import render, redirect
 from django.views.generic.base import TemplateView
+from django.contrib.auth import authenticate, login
 
 from .forms import CreateAccountForm
 
 class HomePageView(TemplateView):
 
-    template_name = 'accounts/index.html'
+    def dispatch(self, request, *args, **kwargs):
+        if request.user.is_authenticated():
+            return render(request, 'accounts/index.html')
 
-def create(request):
+        return render(request, 'index.html')
+
+def signup(request):
     if request.method == 'POST':
         form = CreateAccountForm(request.POST, label_suffix='')
         if form.is_valid():
-            user = form.save()
-            # send mail
-            # log user in
-            # redirect
-            return redirect('home')
+            form.save()
+
+            username = form.cleaned_data['email']
+            password = form.cleaned_data['password']
+
+            user = authenticate(username=username, password=password)
+            if user is not None:
+                login(request, user)
+                return redirect('home')
     else:
         form = CreateAccountForm(label_suffix='')
 
