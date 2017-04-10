07d90df912a7c36c192b90b6ae9ea9fc11889aea
Oladayo Osikoya <alwaysdeone@gmail.com>
2017-04-10 10:46:11 +0000

    Create investment and display success page.

--- core/views.py
+++ core/views.py
@@ -1,15 +1,17 @@
 from django.shortcuts import render
 from django.views.generic.list import ListView
-from django.views.generic.edit import FormView
+from django.views.generic.base import TemplateView
 
 from .models import Investment, Plan
-from .forms import InvestmentForm
 
 class PlanListView(ListView):
     model = Plan
     context_object_name = 'plans'
 
-class InvestView(FormView):
-    model = Investment
-    form_class = InvestmentForm
-    template_name = 'core/invest.html'
+class InvestView(TemplateView):
+    template_name = 'core/invest_success.html'
+
+    def dispatch(self, request, *args, **kwargs):
+        plan = Plan.objects.get(name=kwargs['plan'])
+        investment = Investment.objects.create(user=request.user, plan=plan, amount=plan.deposit)
+        return render(request, self.template_name, {'investment': investment})
