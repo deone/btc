dbe0a9b1e1c4b31894e05f46440c1492a0044ed7
Oladayo Osikoya <alwaysdeone@gmail.com>
2017-04-24 13:39:24 +0000

    Change field widget to TextInput.

--- core/forms.py
+++ core/forms.py
@@ -1,12 +1,15 @@
 from django import forms
 from django.contrib.auth.models import User
+from django.utils.translation import ugettext_lazy as _
 
 from .models import Investment, Plan
 
 class InvestmentForm(forms.ModelForm):
+    plan = forms.CharField(max_length=100, widget=forms.TextInput(attrs={'class': 'form-control', 'readonly': True}))
+
     class Meta:
         model = Investment
-        exclude = ['time_created']
+        exclude = ['plan', 'time_created']
 
     def __init__(self, *args, **kwargs):
         self.user = kwargs.pop('user')
@@ -16,9 +19,14 @@ class InvestmentForm(forms.ModelForm):
         self.label_suffix = ''
 
         self.fields['user'].initial = self.user
-        self.fields['plan'].initial = self.plan
-        self.fields['amount'].initial = self.plan.deposit
+        self.fields['plan'].initial = str(self.plan.deposit) + ' BTC'
+        self.fields['plan'].label = _('Deposit')
 
         self.fields['user'].widget = forms.HiddenInput()
-        self.fields['plan'].widget = forms.HiddenInput()
-        self.fields['amount'].widget = forms.TextInput(attrs={'class': 'form-control', 'readonly': True})
+
+    def save(self, commit=True):
+        deposit = self.cleaned_data['plan'].split(' ')[0]
+        plan = Plan.objects.get(deposit=deposit)
+        self.instance.plan = plan
+
+        return super(InvestmentForm, self).save(commit)
